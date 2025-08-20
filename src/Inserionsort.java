97b94aa756ab2cc0c28bc37db11bf09c7924ee05
Pratapvishwajeet <pratapvishwajeet4@gmail.com>
2025-08-20 20:30:05 +0530

    init commit

--- src/Inserionsort.java
+++ src/Inserionsort.java
@@ -1,2 +1,22 @@
 public class Inserionsort {
+    static void insertionsort(int []a){
+        int n=a.length;
+        for(int i=1;i<n;i++){
+            int j=i;
+            while(j>0 &&a[j]<a[j-1]){
+                int temp=a[j];
+                a[j]=a[j-1];
+                a[j-1]=temp;
+                j--;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int a[]={8,6,4,7,3};
+        insertionsort(a);
+        for(int i:a){
+            System.out.print(i+" ");
+        }
+
+    }
 }
