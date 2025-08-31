a51a68ffb1157ecfb43f4e85818c06ff20c3d955
Pratapvishwajeet <pratapvishwajeet4@gmail.com>
2025-08-31 22:29:42 +0530

    init commit

--- src/7/BINARYSEARCH LEETCODE/leetcode74.java
+++ src/7/BINARYSEARCH LEETCODE/leetcode74.java
@@ -1,22 +1,22 @@
-package 7.BINARYSEARCH LEETCODE;
-
-class Solution {
-    public boolean searchMatrix(int[][] a, int target) {
-        int n=a.length,m=a[0].length;
-        int st=0,end=m*n-1;
-        while(st<=end){
-            int mid=st +(end-st)/2;
-            int midel=a[mid/m][mid%m];
-            if(midel==target)return true;
-            if(target<=midel){
-                end=mid-1;
-            }else{
-                st=mid+1;
-            }
-        }return false;
-
-
-    }
-}
-//public class leetcode74 {
+//package 7.BINARYSEARCH LEETCODE;
+//
+//class Solution {
+//    public boolean searchMatrix(int[][] a, int target) {
+//        int n=a.length,m=a[0].length;
+//        int st=0,end=m*n-1;
+//        while(st<=end){
+//            int mid=st +(end-st)/2;
+//            int midel=a[mid/m][mid%m];
+//            if(midel==target)return true;
+//            if(target<=midel){
+//                end=mid-1;
+//            }else{
+//                st=mid+1;
+//            }
+//        }return false;
+//
+//
+//    }
 //}
+////public class leetcode74 {
+////}
---
+++ src/8/string/pta.java
@@ -0,0 +1,14 @@
+//package string;
+//
+//public class pta {
+//    public static void main(String[] args) {
+//
+//
+//        String str = "hello";
+//        String str2 = "nothello";
+//        str2=str2+10;
+//        System.out.println(str.startsWith("he"));
+//        System.out.println(str2.startsWith("he"));
+//        System.out.println(str.concat(str2));
+//    }
+//}
